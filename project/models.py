bf526067630b328bbd89859db22c6a9ae6fbe2e9
Moria-Mel <bershovamaria@gmail.com>
2023-08-03 04:54:28 +0300

    db update

--- project/models.py
+++ project/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Portfolio(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    short_description = models.TextField()
+    photo_id = models.IntegerField(null=True, blank=True)
+    url_id = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('portfolio-detail', args=[str(self.id)])
+
+
