39751574dde24eebdc417b1ea602db6483b6fad2
Gesha <geshamat1egogey@mail.ru>
2023-08-07 17:47:25 +0300

    gesha's db fixed

--- Tektumharm/settings.py
+++ Tektumharm/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db',
         'USER': 'root',
-        'PASSWORD': 'MelCat5129!',
+        'PASSWORD': '0000',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
